78e3d8ea7ce9ee8861a05aa062a85c9ebf7fff1e
williamdazet <wdazet2@lsu.edu>
2016-03-15 13:01:47 -0500

    Added files via upload

--- demo/js/arcgis.js
+++ demo/js/arcgis.js
@@ -1,7 +1,41 @@
-require(["esri/map", "dojo/domReady!"], function(Map) {
+require(["esri/map", "esri/layers/ArcGISDynamicMapServiceLayer","esri/dijit/BasemapToggle","dojo/domReady!"], function(Map,ArcGISDynamicMapServiceLayer,BasemapToggle){
   var map = new Map("map", {
-    center: [-118, 34.5],
-    zoom: 8,
+    center: [-90, 30],
+    zoom: 4,
     basemap: "topo"
   });
+
+
+  /*
+   * Add layer(s) from an ArcGIS web service
+   *
+   * function name: ArcGISDynamicMapServiceLayer
+   * module name: "esri/layers/ArcGISDynamicMapServiceLayer"
+   * documentation: https://developers.arcgis.com/javascript/jssamples/map_dynamic.html
+   * example: http://developers.arcgis.com/javascript/samples/map_dynamic/
+   */
+
+
+  // Create a layer object from an ArcGIS Server web service, setting the opacity option
+  var layer1 = new ArcGISDynamicMapServiceLayer( "http://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer", {
+    "opacity": 1
+
+  });
+  map.addLayer(layer1); // add the layer object to the map
+
+  /*
+   * Adds a map control to select a basemap. Requires a div with id="BasemapToggle" inside the map div.
+   *
+   * function name: BasemapToggle
+   * module name: "esri/dijit/BasemapToggle"
+   * documentation: https://developers.arcgis.com/javascript/jssamples/widget_toggle.html
+   * example: http://developers.arcgis.com/javascript/samples/widget_toggle/
+   */
+
+  var toggle = new BasemapToggle({
+    map: map,
+    basemap: "hybrid"
+  }, "BasemapToggle");
+  toggle.startup();
+
 });
--- demo/js/googlemaps.js
+++ demo/js/googlemaps.js
@@ -11,5 +11,32 @@ function initMap() {
   });
 
   // code to add layers goes here
+  var kml1 = new google.maps.KmlLayer({
+    url: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week_depth_animated_link.kml',
+    preserveViewport: true // setting to true will prevent the map from zooming to this layer
+  });
+  kml1.setMap( map );
 
+var kml2 = new google.maps.KmlLayer({
+  url: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week_age_animated_link.kml',
+  preserveViewport: true // setting to true will prevent the map from zooming to this layer
+});
+kml2.setMap( map );
+
+
+var kml3 = new google.maps.KmlLayer({
+  url: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month_depth_animated_link.kml',
+  preserveViewport: true // setting to true will prevent the map from zooming to this layer
+});
+kml3.setMap( map );
 }
+
+
+
+
+/*
+ * Add layer(s) from an online KML or KMZ file
+ *
+ * function: google.maps.KmlLayer
+ * documentation: https://developers.google.com/maps/documentation/javascript/examples/layer-kml
+ */
